bbb3e89dfa9887a45a66d04c137c9a3a4498718c
lucore32 <lucore32@kingshigh.school.nz>
2026-06-03 11:01:14 +1200

    Basic movement complete

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -1,25 +1,19 @@
 extends CharacterBody2D
 
+const GRAVITY : int = 4200
+const JUMP_SPEED : int = -1800
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	if is_on_floor():
+		$RunCol.disabled = false
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = JUMP_SPEED
+		elif Input.is_action_pressed("ui_down"):
+			$AnimatedSprite2D.play("crouch")
+			$RunCol.disabled = true
+		else:
+			$AnimatedSprite2D.play("run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		$AnimatedSprite2D.play("jump")
 	move_and_slide()
