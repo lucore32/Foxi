13d27f70e2fb22fdaa2abb683c222b6e934549c7
lucore32 <lucore32@kingshigh.school.nz>
2026-06-16 14:02:46 +1200

    Remaking ground and exporting for the game jam

---
+++ foxi-(4.4)/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+const GRAVITY : int = 4200
+const JUMP_SPEED : int = -1300
+
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	if is_on_floor():
+		if not get_parent().game_running:
+			$AnimatedSprite2D.play("Idle")
+		if not get_parent().game_running:
+			pass
+		else:
+			$RunCol.disabled = false
+			if Input.is_action_pressed("ui_accept"):
+				velocity.y = JUMP_SPEED
+			elif Input.is_action_pressed("ui_down"):
+				$AnimatedSprite2D.play("crouch")
+				$RunCol.disabled = true
+			else:
+				$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("jump")
+	move_and_slide()
