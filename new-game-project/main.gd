ee6ee800aef699860698a07a88f332a9b8d3fc22
lucore32 <lucore32@kingshigh.school.nz>
2026-06-24 14:54:46 +1200

    multiple spawning obstacles

--- new-game-project/main.gd
+++ new-game-project/main.gd
@@ -3,48 +3,45 @@ extends Node
 var rock_scene = preload("res://rock.tscn")
 var crate_scene = preload("res://crate.tscn")
 var bird_scene = preload("res://bird.tscn")
-var obstacle_types := [rock_scene, crate_scene, bird_scene]
+var obstacle_types := [rock_scene, crate_scene]
 var obstacles : Array
 var bird_heights := [200, 390]
 
-const FOX_START_POS := Vector2i(100, 640)
+
+const FOX_START_POS := Vector2i(150, 485)
 const CAM_START_POS := Vector2i(576, 324)
 var score : int
-const SCORE_MODIFIER : int = 500
+const SCORE_MODIFIER : int = 10
 var speed : float
-const START_SPEED : float = 9.0
-const MAX_SPEED : float = 25.0 
-const SPEED_MODIFIER : int = 8000
+const START_SPEED : float = 6.0
+const MAX_SPEED : float = 12.0
+const SPEED_MODIFIER : int = 5000
 var screen_size : Vector2i
 var ground_height : int
 var game_running : bool
-var last_obs	
+var last_obs
+
 
 func _ready():
 	screen_size = get_window().size
-
-# Point to the TileMap child node inside $Ground
-	var tilemap = $Ground/TileMap 
-
-	var tile_size = tilemap.tile_set.tile_size
-	var used_rect = tilemap.get_used_rect()
-
-	ground_height = used_rect.size.y * tile_size.y
-
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
-
+	
 func new_game():
 	score = 0
+	show_score()
 	game_running = false
+	
 	$Player.position = FOX_START_POS
+	$Player.velocity = Vector2i(0,0)
 	$Camera2D.position = CAM_START_POS
 	$Ground.position = Vector2i(0, 0)
-	# Initialize speed at the start of the game
-	speed = START_SPEED 
+	
 	$HUD.get_node("StartLabel").show()
 
 func _process(delta):
 	if game_running:
+		print(speed)
 		speed = START_SPEED + score / SPEED_MODIFIER
 		if speed > MAX_SPEED:
 			speed = MAX_SPEED
@@ -53,17 +50,15 @@ func _process(delta):
 		
 		
 		
-		var movement = speed * delta * 60 
-		
-		$Player.position.x += movement
-		$Camera2D.position.x += movement
+		$Player.position.x += speed
+		$Camera2D.position.x += speed
 		
 		score += speed
 		show_score()
 		
-		# 2. Infinite ground scrolling logic
-		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
-			$Ground.position.x += screen_size.x
+		if $Camera2D.position.x - $Ground.position.x >  screen_size.x * 1.5:
+			$Ground.position.x += screen_size.x 
+			
 	else:
 		if Input.is_action_pressed("ui_accept"):
 			game_running = true
@@ -71,17 +66,24 @@ func _process(delta):
 
 func generate_obs():
 	
-	if obstacles.is_empty():
+	if obstacles.is_empty() or last_obs.position.x < score + randi_range(300,500):
 		var obs_type = obstacle_types[randi() % obstacle_types.size()]
 		var obs
-		obs = obs_type.instantiate()
-		var obs_height = obs.get_node("Sprite2D").texture.get_height()
-		var obs_scale = obs.get_node("Sprite2D").scale
-		var obs_x : int = screen_size.x + score + 100
-		var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y / 2) + 5
-		last_obs = obs
-		add_child(obs)
-		obstacles.append(obs)
+		var max_obs = 3
+		for i in range(randi() %  max_obs + 1):
+			obs = obs_type.instantiate()
+			var obs_height = obs.get_node("Sprite2D").texture.get_height()
+			var obs_scale = obs.get_node("Sprite2D").scale
+			var obs_x : int = screen_size.x + score + 100 + (1 * 100)
+			var obs_y : int = screen_size.y - ground_height - (obs_height * obs_scale.y / 2) + 5
+			last_obs = obs
+			add_obs(obs, obs_x, obs_y)
+
 	
+func add_obs(obs, x, y):
+	obs.position = Vector2i(x, y)                                                                                              
+	add_child(obs)
+	obstacles.append(obs)
+
 func show_score():
 	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
